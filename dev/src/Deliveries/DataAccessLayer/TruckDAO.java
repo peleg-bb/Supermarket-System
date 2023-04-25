6f2389c615c8f7f6a48796c317f0577b4f5c53c9
Peleg <benbapel@post.bgu.ac.il>
2023-04-25 20:36:53 +0300

    :beers: drunk implementation of truckDAO

---
+++ dev/src/Deliveries/DataAccessLayer/TruckDAO.java
@@ -0,0 +1,38 @@
+package Deliveries.DataAccessLayer;
+
+import Deliveries.BusinessLayer.Driver;
+import Deliveries.BusinessLayer.Truck;
+import HR.DataAccessLayer.Connect;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class TruckDAO {
+    private final Connect conn;
+    public TruckDAO() {
+        conn = Connect.getInstance();
+    }
+
+    public Set<Truck> loadData(){
+        Set<Truck> trucks = new HashSet<>();
+        try {
+            List<HashMap<String, Object>> truckDetails = conn.executeQuery("SELECT * FROM Trucks");
+            for (HashMap<String, Object> truckRecord: truckDetails) {
+                Truck truck = getTruck(truckRecord);
+                trucks.add(truck);
+            }
+            return trucks;
+        }
+        catch (SQLException exception) {
+            return null;
+        }
+    }
+
+    private Truck getTruck(HashMap<String, Object> driverRecord) {
+        throw new UnsupportedOperationException();
+    }
+
+}
