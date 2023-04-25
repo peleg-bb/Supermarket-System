f1379eb20fe4bfca5caa5d68c6e2bcef44900464
Peleg <benbapel@post.bgu.ac.il>
2023-04-25 12:56:28 +0300

    :hammer: updated integration interface

--- dev/src/HR_Deliveries_Interface/DeliveryIntegrator.java
+++ dev/src/HR_Deliveries_Interface/DeliveryIntegrator.java
@@ -1,10 +1,9 @@
 package HR_Deliveries_Interface;
 
 import Deliveries.BusinessLayer.DeliveryStop;
-
 import java.sql.Timestamp;
 import java.util.Set;
 
 public interface DeliveryIntegrator {
-    Set<DeliveryStop> getDeliverygetDeliveryByArrivalTime(Timestamp arrivalTime, String store);
+    Set<DeliveryStop> getDeliverygetDeliveryByArrivalTime(Timestamp startTime, Timestamp finishTime, String store);
 }
