1d3c6604996cd481cbffab477fb6a5fa7745d69a
Peleg <benbapel@post.bgu.ac.il>
2023-05-09 13:57:04 +0300

    :white_check_mark: created test

--- dev/src/Deliveries/DeliveriesTests/DriverDAOTest.java
+++ dev/src/Deliveries/DeliveriesTests/DriverDAOTest.java
@@ -1,20 +1,21 @@
 package Deliveries.DeliveriesTests;
 
-import Deliveries.BusinessLayer.Driver;
-import Deliveries.BusinessLayer.DriverController;
-import Deliveries.BusinessLayer.Site;
-import Deliveries.BusinessLayer.SiteGenerator;
+import Deliveries.BusinessLayer.*;
+import Deliveries.BusinessLayer.Enums_and_Interfaces.DeliveryException;
 import Deliveries.DataAccessLayer.DriverDAO;
 import Deliveries.DataAccessLayer.SiteDAO;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Timestamp;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class DriverDAOTest {
     DriverDAO driverDAO;
@@ -30,12 +31,17 @@ class DriverDAOTest {
 
     @Test
     void generateDriversToDB() {
-        SiteGenerator siteGenerator = new SiteGenerator();
-        List<Site> sites = siteGenerator.getSitesList();
-        SiteDAO siteDAO = new SiteDAO();
-        for (Site site : sites) {
-           assertTrue(siteDAO.saveSite(site));
+        DriverController driverController = DriverController.getInstance();
+        Timestamp timestamp = new Timestamp(2023-1900, 6, 4, 10, 0, 0, 0);
+        Timestamp timestamp2 = new Timestamp(2023-1900, 6, 4, 12, 0, 0, 0);
+        Truck truck = mock(Truck.class);
+
+        try {
+           driverController.pickDriver(truck, timestamp, timestamp2);
+        } catch (DeliveryException e) {
+            throw new RuntimeException(e);
         }
+
     }
 
     @BeforeEach
