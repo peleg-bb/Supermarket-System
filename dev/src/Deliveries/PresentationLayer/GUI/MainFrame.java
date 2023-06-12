23fade6c96e2ba7749662fd3d34fcbd27bf8b373
Peleg <benbapel@post.bgu.ac.il>
2023-06-12 15:39:18 +0300

    :construction: basic GUI buttoms

--- dev/src/Deliveries/PresentationLayer/GUI/MainFrame.java
+++ dev/src/Deliveries/PresentationLayer/GUI/MainFrame.java
@@ -13,9 +13,16 @@ public class MainFrame extends JFrame {
         JLabel label = new JLabel("Welcome to the Deliveries System!");
         add(label);
         JButton addStopButton = new JButton("Add a delivery stop");
+        addStopButton.setBounds(50, 100, 95, 30);
+        addStopButton.setVisible(true);
         JButton removeStopButton = new JButton("Remove a delivery stop");
+        removeStopButton.setBounds(50, 100, 95, 30);
+        removeStopButton.setVisible(true);
         JButton executeButton = new JButton("Execute a delivery");
+        executeButton.setBounds(50, 100, 95, 30);
+        executeButton.setVisible(true);
         JButton exitButton = new JButton("Exit");
+        exitButton.setBounds(150, 200, 95, 30);
 
         add(addStopButton);
         add(removeStopButton);
