0a70c7be12dacc20a4c125423188ab4cdb6fc4cd
Peleg <benbapel@post.bgu.ac.il>
2023-06-12 15:35:21 +0300

    :construction: added basic GUI framework

--- dev/src/Deliveries/PresentationLayer/GUI/MainFrame.java
+++ dev/src/Deliveries/PresentationLayer/GUI/MainFrame.java
@@ -1,4 +1,26 @@
 package Deliveries.PresentationLayer.GUI;
 
-public class MainFrame {
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame() {
+        super("Deliveries System");
+        // add an explanatory title
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setVisible(true);
+        // Add labels and buttons here
+        JLabel label = new JLabel("Welcome to the Deliveries System!");
+        add(label);
+        JButton addStopButton = new JButton("Add a delivery stop");
+        JButton removeStopButton = new JButton("Remove a delivery stop");
+        JButton executeButton = new JButton("Execute a delivery");
+        JButton exitButton = new JButton("Exit");
+
+        add(addStopButton);
+        add(removeStopButton);
+        add(executeButton);
+        add(exitButton);
+    }
+
 }
--- dev/src/Deliveries_HR_Main.java
+++ dev/src/Deliveries_HR_Main.java
@@ -10,15 +10,13 @@ public class Deliveries_HR_Main {
         System.out.println("Welcome! Please choose which system you'd like to enter:");
         System.out.println("1. HR System");
         System.out.println("2. Deliveries System");
+        System.out.println("3. Deliveries GUI");
         Scanner scanner = new Scanner(System.in);
         String option = scanner.nextLine();
         switch (option) {
-            case "1" -> {
-                Menu.main(args);
-            }
-            case "2" -> {
-                CLI.main(args);
-            }
+            case "1" -> Menu.main(args);
+            case "2" -> CLI.main(args);
+            case "3" -> new Deliveries.PresentationLayer.GUI.MainFrame();
         }
     }
 
