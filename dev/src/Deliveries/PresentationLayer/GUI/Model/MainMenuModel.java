063f59e3d6294134d7616d821ac9e8b2cad08d43
Peleg <benbapel@post.bgu.ac.il>
2023-06-13 14:29:19 +0300

    :construction_worker: added basic Model structure

--- dev/src/Deliveries/PresentationLayer/GUI/Model/MainMenuModel.java
+++ dev/src/Deliveries/PresentationLayer/GUI/Model/MainMenuModel.java
@@ -1,10 +1,29 @@
 package Deliveries.PresentationLayer.GUI.Model;
 
+import Deliveries.BusinessLayer.DeliveryFormsController;
+import Deliveries.BusinessLayer.DeliveryManagerImpl;
+import Deliveries.BusinessLayer.Generators.SiteGenerator;
+import Deliveries.BusinessLayer.Site;
+
 import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.Scanner;
 
 public class MainMenuModel implements ActionListener {
+    private DeliveryManagerImpl deliveryManager = DeliveryManagerImpl.getInstance();
+    private DeliveryFormsController deliveryFormsController;
+    public MainMenuModel() {
+        deliveryManager = DeliveryManagerImpl.getInstance(); //removed the use of service class
+        deliveryFormsController = deliveryManager.getDeliveryFormsController();
+        deliveryFormsController.loadFormsData();
+    }
     @Override
     public void actionPerformed(java.awt.event.ActionEvent e) {
+        deliveryManager.createDeliveryGroup();
+        if (!deliveryManager.getDeliveryFormsController().getPendingDeliveryForms().iterator().hasNext()) {
+            System.out.println("Couldn't create delivery groups due to an illegal combination of delivery stops");
+            return;
+        }
 
     }
 }
--- dev/src/Deliveries/PresentationLayer/GUI/View/MainFrame.java
+++ dev/src/Deliveries/PresentationLayer/GUI/View/MainFrame.java
@@ -1,10 +1,16 @@
 package Deliveries.PresentationLayer.GUI.View;
 
+import Deliveries.BusinessLayer.Generators.SiteGenerator;
+import Deliveries.BusinessLayer.Site;
 import Deliveries.PresentationLayer.GUI.Model.MainMenuModel;
+
+import java.util.List;
 
 public class MainFrame extends AbstractFrame {
     public MainFrame() {
         super(4, new MainMenuModel());
+        SiteGenerator siteGenerator = new SiteGenerator();
+        List<Site> sitesList = siteGenerator.getSitesList(); // Not sure if needed
         addBottom(e -> {}, "Add a delivery stop");
         addBottom(e -> {}, "Remove a delivery stop");
         addBottom(e -> {}, "Execute a delivery");
