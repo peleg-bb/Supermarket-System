5b1ea327ea27a9216eeb1853082516b20cd60d70
Peleg <benbapel@post.bgu.ac.il>
2023-06-17 19:40:21 +0300

    :children_crossing: added different run configurations

--- dev/src/Deliveries_HR_Main.java
+++ dev/src/Deliveries_HR_Main.java
@@ -2,6 +2,7 @@ import Deliveries.BusinessLayer.DeliveryFormsController;
 import Deliveries.BusinessLayer.DeliveryManagerImpl;
 import Deliveries.PresentationLayer.CLI.CLI;
 import Deliveries.PresentationLayer.GUI.View.MainMenuFrame;
+import HR.PresentationLayer.GUI.View.LoginFrame;
 import HR.PresentationLayer.Menu;
 
 import javax.swing.*;
@@ -13,10 +14,15 @@ public class Deliveries_HR_Main {
 
     public static void main(String[] args) throws SQLException {
         loadData();
-        if (Objects.equals(args[0], "GUI")) {
+        if (Objects.equals(args[0], "GUI") && Objects.equals(args[1], "Deliveries")) {
             SwingUtilities.invokeLater(MainMenuFrame::new);
-        }
-        else {
+        } else if (Objects.equals(args[0], "GUI") && Objects.equals(args[1], "HR")) {
+            SwingUtilities.invokeLater(LoginFrame::new);
+        } else if (Objects.equals(args[0], "CLI") && Objects.equals(args[1], "Deliveries")) {
+            CLI.main(args);
+        } else if (Objects.equals(args[0], "CLI") && Objects.equals(args[1], "HR")) {
+            Menu.main(args);
+        } else {
             System.out.println("Welcome! Please choose which system you'd like to enter:");
             System.out.println("1. HR System");
             System.out.println("2. Deliveries System");
